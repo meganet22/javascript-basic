7615f639a96bd99ed943bc37eb4a76fe03f8ea17
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-04 05:52:40 +0300

    javascript-basic

---
+++ 1_all_types_of_popup_boxes.js
@@ -0,0 +1,16 @@
+alert
+confirm
+prompt
+
+It is used to prevent the page from refreshing
+Purpose of void (0);
+
+
+Events are things that happen to HTML elements
+*Web page has finished loading
+*Input field was changed
+*Button was clicked
+What are Events
+
+This method is used to call a function or evaluate an expression at specified intervals
+setInterval
