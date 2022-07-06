fe08a3d81d73e7cd034bad05c6d483c7c27a0331
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-06 04:35:40 +0300

    Create 8_events.js

---
+++ 8_events.js
@@ -0,0 +1,3 @@
+Olaylar 
+HTML elementleri üzerlerinde bir işlem gerçekleştiğinde bunu  bildirmek için olaylar tetiklerler. Örneğin bir düğmeye tıklanması,  klavyeden bir tuşa basılması,sayfanın içeriğinin yüklenmesi gibi  durumlar elementlerin tetiklediği olaylar tarafından JS’ye bildirilir.  JS bu olaylara tepki verecek şekilde programlanabilir. Buamaçla elementlerin sağladığı olaylara JS fonksiyonları bağlanır. 
+Örnek onclick, onload 
