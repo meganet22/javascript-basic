42478ea71d3bc4496e779a3f7f5cc0b8fa6aa898
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-06 05:21:23 +0300

    Update 10_operands.js

--- 10_operands.js
+++ 10_operands.js
@@ -3,3 +3,10 @@
 3.	Mantıksal işleçler ne için kullanılır? Cevap: Mantıksal işleçler verilen ifadenin doğruluğunu sınar.
 4.	JS dilinde ‘!==’ ifadesi ne anlama gelir? Cevap: Değer veya tür eşit değildir
 5.	JS dilindeki || ifadesinin görevi nedir? Cevap: VEYA (OR) işleci erilen ifadelerden birinin doğru olması halinde true değeri çıkarır. Aksi halde false çevirir.
+
+
+Aritmetik İşleçler: Değerler arasında işlem yapmak içinaritmetik işleçler kullanılabilir. Örneğin; +değerleri toplar, * verilen değerleri çarpar, % ikinci değeregöre ilk değerin modunu alır.verilen 
+
+Atama İşleçleri: Atama işleçleri değişkenlere verigöndermek için kullanılır. Bu işleçlerin kullanılabilmesiiçin ifadenin sol tarafında bir değişken bulunması gerekir.Metin İşleci: + işleci metinlerin birleştirilmesi için dekullanılır. Arasına + işleci konan metinler birleştirilir. 
+Kıyaslama İşleçleri ve Mantıksal İşleçlerMantıksal işleçler verilen ifadenin doğruluğunu sınar. Buifadelerin sonuçları true (doğru) ya da false (yanlış) olur.Kıyaslama ve mantıksal işleçler genellikle karar yapılarıile birlikte kullanılır. Örneğin; <= işleci küçük ya da eşittiranlamındadır. 2<-3 işlemi true sonucunu verecektir. 
+
