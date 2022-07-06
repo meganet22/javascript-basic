f66ef93edb46cfc31692cfdab013e965a7db491f
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-06 04:46:31 +0300

    Create 11_series.js

---
+++ 11_series.js
@@ -0,0 +1,10 @@
+Dizilerle çalışırken kullanılabilecek önemli metotlardan biri olan shift in görevi aşağıdakilerden hangisinde doğru verilmiştir?
+
+Dizideki ilk elemanı çıkarır.
+
+Push metodu, dizinin sonuna yeni eleman ekler. 
+Pop dizideki son elemanı çıkarır. 
+Delete diziden verilen indisli elemanı siler. 
+Concat iki diziyi birleştirir. 
+
+shift dizideki ilk elemanı çıkarır. 
