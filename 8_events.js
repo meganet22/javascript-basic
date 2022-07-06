24d65d413d0e38e9e34797a4fc65a999b0859dd9
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-06 04:54:55 +0300

    Update 8_events.js

--- 8_events.js
+++ 8_events.js
@@ -1,5 +1,26 @@
 Olaylar 
-HTML elementleri üzerlerinde bir işlem gerçekleştiğinde bunu  bildirmek için olaylar tetiklerler. Örneğin bir düğmeye tıklanması,  klavyeden bir tuşa basılması,sayfanın içeriğinin yüklenmesi gibi  durumlar elementlerin tetiklediği olaylar tarafından JS’ye bildirilir.  JS bu olaylara tepki verecek şekilde programlanabilir. Buamaçla elementlerin sağladığı olaylara JS fonksiyonları bağlanır. 
+HTML elementleri üzerlerinde bir işlem gerçekleştiğinde bunu  bildirmek için olaylar tetiklerler. 
+Örneğin bir düğmeye tıklanması,  klavyeden bir tuşa basılması,sayfanın içeriğinin yüklenmesi gibi  durumlar elementlerin tetiklediği olaylar tarafından JS’ye bildirilir.  JS bu olaylara tepki verecek şekilde programlanabilir. 
+Buamaçla elementlerin sağladığı olaylara JS fonksiyonları bağlanır. 
 Örnek onclick, onload 
 
 Javascript’te HTML DOM’un sağladığı olaylardan hangisi kullanıcının klavyeden bir tuşa basması anında tetiklenmeyi sağlar =  onkeydown
+
+mouseleave: Farenin elementin üzerinden ayrılması Klavye Olayları keypress: Kullanıcının klavyeden bir tuşa basarak elini çekmesi 
+
+keydown: Kullanıcının klavye tuşuna basması keyup: Kullanıcının klavye tuşundan elini çekmesi Form Olayları 
+
+submit: Formun submit düğmesi ile gönderilmesi 
+
+change: Bir elementin değerinin değişmesi 
+
+focus: Bir elementin klavye odağına alınması 
+
+blur: Klavye odağının elementten ayrılması Pencere Olayları 
+
+load: Bir elementin tüm alt elementlerinin yüklenmesinin tamamlanması 
+
+resize: Tarayıcı penceresinin boyutlandırılması 
+
+unload: Pencerenin farklı bir internet adresine gitmesi 
+
