e70eaf0a6807b3ede3aa0cab3ec9caad55553df3
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-06 04:40:54 +0300

    Create 10_operands.js

---
+++ 10_operands.js
@@ -0,0 +1,5 @@
+1.	JS işleçleri nelerdir? Cevap: JS işleçleri aritmetik işleçler, atama işleçleri, metin işleci ve kıyaslama ve mantıksal işleçler şeklinde sınıflanabilir.
+2.	Atama işleçlerinin kullanım amacı nedir? Cevap: Atama işleçleri değişkenlere veri göndermek için kullanılır.
+3.	Mantıksal işleçler ne için kullanılır? Cevap: Mantıksal işleçler verilen ifadenin doğruluğunu sınar.
+4.	JS dilinde ‘!==’ ifadesi ne anlama gelir? Cevap: Değer veya tür eşit değildir
+5.	JS dilindeki || ifadesinin görevi nedir? Cevap: VEYA (OR) işleci erilen ifadelerden birinin doğru olması halinde true değeri çıkarır. Aksi halde false çevirir.
