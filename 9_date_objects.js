25c134ae7c752adff9c57d26fc8cbc5aa268538f
Can Ata <79902326+meganet22@users.noreply.github.com>
2022-07-06 04:37:00 +0300

    Create 9_date_objects.js

---
+++ 9_date_objects.js
@@ -0,0 +1,15 @@
+Tarih Nesnesi 
+JS dilinde tarih işlemlerini gerçekleştirmek için date nesnesi  kullanılır. 
+
+date nesnesi oluşturmak için new anahtar sözcüğü kullanılır.  B
+
+unun ardından nesneninmetotları kullanılarak, tarih nesnesinden  istenen bilgiler çekilebilir. 
+
+Aşağıda verilen kodda, ikinci satırda bir  date nesnesi oluşturularak tarih değişkenine atanmaktadır.
+
+Üçüncü  satırda getMinutes metotları ile saat çekilmektedir. 
+
+Bu bilgiler  tarayıcı konsoluna mesaj olarak yazdırılmaktadır. 
+
+
+date nesnesinin  getHours veya getMinutes bilgileri ise 
